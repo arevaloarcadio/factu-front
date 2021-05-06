04f12a367f6c0fb6423412d35eeb6df292743059
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2021-05-06 08:55:55 -0400

    modulo para eliminado forzado de cache

--- src/main.js
+++ src/main.js
@@ -19,7 +19,42 @@ import VueNotifications from 'vue-notifications';
 import miniToastr from 'mini-toastr';
 //Paginacion con vue
 import VuePaginate from 'vue-paginate'
+
+import { register } from "register-service-worker";
+
+if (process.env.NODE_ENV === "production") {
+  register(`${process.env.BASE_URL}firebase-messaging-sw.js`, {
+    registered() {
+      console.log("Service worker has been registered.");
+    },
+    cached() {
+      console.log("Content has been cached for offline use.");
+    },
+    updatefound() {
+      // new content clear cache so user gets the new version
+      caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+          caches.delete(cacheName);
+        });
+      });
+      console.log("New content is downloading.");
+    },
+    updated() {
+      console.log("New content is available; please refresh.");
+    },
+    offline() {
+      console.log(
+        "No internet connection found. App is running in offline mode."
+      );
+    },
+    error(error) {
+      console.error("Error during service worker registration:", error);
+    }
+  });
+}
+
 Vue.use(VuePaginate)
+
 
 const toastTypes = {
     success: 'success',
