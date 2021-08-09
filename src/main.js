080b17d54a303cc6ffcba3c602a94795212e8f22
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2021-08-09 13:43:53 -0400

    push

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ import VuePaginate from 'vue-paginate'
 
 import { register } from "register-service-worker";
 
+
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}firebase-messaging-sw.js`, {
     registered() {
